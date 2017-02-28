291edcd4b621cad8582ac2fb236854b579a77c28
shaman2001 <Vladimir_Kotovich@epam.com>
2017-02-28 11:00:35 +0300

    project skeleton

---
+++ src/test/java/com/epam/AirBaltic/TestNgTestBase.java
@@ -0,0 +1,46 @@
+package com.epam.AirBaltic;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Capabilities;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import ru.stqa.selenium.factory.WebDriverPool;
+
+import com.epam.AirBaltic.util.PropertyLoader;
+
+/**
+ * Base class for TestNG-based test classes
+ */
+public class TestNgTestBase {
+
+  protected static String gridHubUrl;
+  protected static String baseUrl;
+  protected static Capabilities capabilities;
+
+  protected WebDriver driver;
+
+  @BeforeSuite
+  public void initTestSuite() throws IOException {
+    baseUrl = PropertyLoader.loadProperty("site.url");
+    gridHubUrl = PropertyLoader.loadProperty("grid.url");
+    if ("".equals(gridHubUrl)) {
+      gridHubUrl = null;
+    }
+    capabilities = PropertyLoader.loadCapabilities();
+  }
+
+  @BeforeMethod
+  public void initWebDriver() {
+    driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    WebDriverPool.DEFAULT.dismissAll();
+  }
+}
