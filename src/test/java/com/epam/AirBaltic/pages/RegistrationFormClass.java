9c3a9c46168db263fe8ffa2421f51740d367dbc3
Mark Rudak <Mark_Rudak@epam.com>
2017-03-01 13:35:25 +0300

    Add RegistrationForm class

---
+++ src/test/java/com/epam/AirBaltic/pages/RegistrationFormClass.java
@@ -0,0 +1,13 @@
+package com.epam.AirBaltic.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Mark_Rudak on 3/1/2017.
+ */
+public class RegistrationFormClass extends  Page {
+    
+    public RegistrationFormClass(WebDriver driver) {
+        super(driver);
+    }
+}
