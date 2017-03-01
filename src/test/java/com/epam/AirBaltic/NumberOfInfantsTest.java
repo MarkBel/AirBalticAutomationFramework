f0c7a69f7ed261d0fc0c36fe2bb45c51b1ad8e0d
Mark Rudak <Mark_Rudak@epam.com>
2017-03-01 17:04:31 +0300

    Add Number of infants test

---
+++ src/test/java/com/epam/AirBaltic/NumberOfInfantsTest.java
@@ -0,0 +1,18 @@
+package com.epam.AirBaltic;
+
+import com.epam.AirBaltic.pages.StartPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Mark_Rudak on 3/1/2017.
+ */
+public class NumberOfInfantsTest extends PageTest {
+
+    @Test
+    public void NumberOfInfantsTickets()
+    {
+        Assert.assertTrue(new StartPage(driver).openBookAndFlights().checkNumberInfactsTichets(),"Number of infants can't be higher than number of adults!");
+    }
+
+}
--- src/test/java/com/epam/AirBaltic/pages/BookAndFlightPage.java
+++ src/test/java/com/epam/AirBaltic/pages/BookAndFlightPage.java
@@ -1,5 +1,6 @@
 package com.epam.AirBaltic.pages;
 
+import org.junit.FixMethodOrder;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +16,7 @@ public class BookAndFlightPage extends Page {
 
     private static final String ERROR_MESSAGE = "The date of the inbound flight cannot be earlier than the date of the outbound flight. Please adjust your selection.";
     private static final String RETURN_DATE_ATTRIBUTE = "display: none;";
+    private static final String ERROR_INPUT_EXCEPTION = "The number of infants can not be higher than the number of adults. Only an adult can accompany an infant.";
 
     @FindBy(xpath = "//div[@data-container-id=\"returnDate\"]/input[@id=\"flt_returning_on\"]")
     WebElement inptReturnDate;
@@ -27,6 +29,18 @@ public class BookAndFlightPage extends Page {
 
     @FindBy(xpath = "//div[@id='return-date-div']")
     WebElement elemReturnDate;
+
+    @FindBy(xpath = "//a[@class=\"dropdown-toggle needsclick\" and text()='0 infants']")
+    WebElement listInfants;
+
+    @FindBy(xpath = "//div[@id='mCSB_3_container']/li[3]/a")
+    WebElement numberOfInfants;
+
+    @FindBy(xpath = "//button[@class='btn btn-default light-elem-btn button-green findflights-btn']")
+    WebElement btnBookandFligh;
+
+    @FindBy(xpath = "//td[@id='errors']/span")
+    WebElement inputNumberOfInfantsError;
 
     public BookAndFlightPage(WebDriver driver) {
         super(driver);
@@ -49,6 +63,25 @@ public class BookAndFlightPage extends Page {
 
         return RETURN_DATE_ATTRIBUTE.equals(elemReturnDate.getAttribute("style"));
 
+
+    }
+
+    public boolean checkNumberInfactsTichets() {
+
+        inptReturnDate.sendKeys("10.03.2017");
+        inptReturnDate.sendKeys(Keys.ENTER);
+
+
+        driver.manage().timeouts().implicitlyWait(150, TimeUnit.SECONDS);
+
+        listInfants.click();
+
+        numberOfInfants.click();
+
+        btnBookandFligh.click();
+
+
+        return inputNumberOfInfantsError.getText().contains(ERROR_INPUT_EXCEPTION);
 
     }
 
