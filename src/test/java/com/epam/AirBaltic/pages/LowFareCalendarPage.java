244e63bae7ab07bf1955093ff79a08cb5c734b03
DmitryiPaulioz <dmitryi_paulioz@epam.com>
2017-03-01 18:46:47 +0300

    Add fare page.

--- src/test/java/com/epam/AirBaltic/pages/LowFareCalendarPage.java
+++ src/test/java/com/epam/AirBaltic/pages/LowFareCalendarPage.java
@@ -1,6 +1,8 @@
 package com.epam.AirBaltic.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 /**
@@ -8,8 +10,39 @@ import org.openqa.selenium.support.FindBy;
  */
 public class LowFareCalendarPage extends Page {
 
+    @FindBy(xpath = "//*[@id='your-selection']//span[2]")
+    private WebElement priceField;
+    private String cheepTicketsPageURL = "https://tickets.airbaltic.com/en/book/avail";
+
+    @FindBy(xpath = "//button[@class='btn-continue-booking']")
+    private WebElement cheepPageButton;
+
+    private WebElement accessButton;
+
+
     public LowFareCalendarPage(WebDriver driver) {
         super(driver);
+    }
+
+    private void getTicketsPrice(){
+        System.out.println(priceField.getText());
+    }
+
+    private void clickSkipCheepButton(){
+        cheepPageButton.click();
+    }
+
+    private void clickAcceptButton(){
+        accessButton = driver.findElements(By.xpath("//*[@class='btn-green']")).get(1);
+        accessButton.click();
+    }
+
+    public void confirmBooking(){
+        getTicketsPrice();
+        if(driver.getCurrentUrl().equals(cheepTicketsPageURL)) {
+            clickSkipCheepButton();
+        }
+        clickAcceptButton();
     }
 
 }
