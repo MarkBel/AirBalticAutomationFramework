71aeca440bec17916fee374ce16102ac5d30a0c5
shaman2001 <Vladimir_Kotovich@epam.com>
2017-03-03 12:41:08 +0300

    Methods for tests of SnowyPeaksPage and YouthOfferPage added

--- src/test/java/com/epam/AirBaltic/pages/StartPage.java
+++ src/test/java/com/epam/AirBaltic/pages/StartPage.java
@@ -5,42 +5,74 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Sample page
  */
 public class StartPage extends Page {
 
-  @FindBy(how = How.TAG_NAME, using = "h1")
-  @CacheLookup
-  public WebElement header;
+    @FindBy(how = How.TAG_NAME, using = "h1")
+    @CacheLookup
+    public WebElement header;
 
-  @FindBy(xpath = "//button[@id='flights-form-btn']")
-  WebElement btnBookAndFlights;
+    @FindBy(xpath = "//button[@id='flights-form-btn']")
+    WebElement btnBookAndFlights;
 
-  @FindBy(xpath = "//input[@id='flt_destin_text']")
-  WebElement inptDestTo;
+    @FindBy(xpath = "//input[@id='flt_destin_text']")
+    WebElement inptDestTo;
+
+    @FindBy(css = "a#ancillary-2-en.item.pull-left")
+    public WebElement linkFlySnowyPeaks;  //testcase #6
+
+    @FindBy(css = "div.menu-item.drop-flights")
+    public WebElement linkFlights;  //testcase #4
+
+    //ul/li/a[@href='/youth-offer']
+    @FindBy(xpath = "//div[@class='mega-list']/ul/li/a[@href='/youth-offer']")
+    public WebElement linkYouthOffer;  //testcase #4
 
 
 
+    public StartPage(WebDriver driver) {
+        super(driver);
+    }
 
-  public StartPage(WebDriver driver) {
-    super(driver);
-  }
+    public BookAndFlightPage openBookAndFlights() {
 
-  public BookAndFlightPage openBookAndFlights() {
+        inptDestTo.clear();
+        inptDestTo.sendKeys("Oslo");
 
-    inptDestTo.clear();
-    inptDestTo.sendKeys("Oslo");
+        btnBookAndFlights.click();
 
-    btnBookAndFlights.click();
+        return new BookAndFlightPage(driver);
+    }
 
-    return new BookAndFlightPage(driver);
-  }
+    public BookAFlightForm goToBookAFlightForm() {
+        return new BookAFlightForm(driver);
+    }
 
-  public BookAFlightForm goToBookAFlightForm(){
-    return new BookAFlightForm(driver);
-  }
+    public SnowyPeaksPage clickFlySnowyPeaksLink() {
+        linkFlySnowyPeaks.click();
+        return new SnowyPeaksPage(this.driver);
+    }
+
+    public void clickFlightsLink() {
+        linkFlights.click();
+    }
+
+    public void clickYouthOfferLink() {
+        linkYouthOffer.click();
+    }
+
+    public YouthOfferPage gotoYouthOfferPageByLink() {
+        this.clickFlightsLink();
+        (new WebDriverWait(this.driver, 5)).until(ExpectedConditions.
+                visibilityOf(linkYouthOffer));
+        this.clickYouthOfferLink();
+        return new YouthOfferPage(this.driver);
+    }
+
 
 }
