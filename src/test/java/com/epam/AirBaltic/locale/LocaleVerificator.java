2f3b05c33de8e05d1dd44e4b936a5da155bb97c8
Mark Rudak <Mark_Rudak@epam.com>
2017-03-03 18:40:41 +0300

    Add LocaleVerificator

---
+++ src/test/java/com/epam/AirBaltic/locale/LocaleVerificator.java
@@ -0,0 +1,45 @@
+package com.epam.AirBaltic.locale;
+
+import com.epam.AirBaltic.pages.Page;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Mark_Rudak on 3/3/2017.
+ */
+public class LocaleVerificator  extends Page {
+
+
+    @FindBy(xpath = "//button[@id='dropdownMenu2']")
+    WebElement fieldLang;
+
+    @FindBy(xpath = "//ul[@class='dropdown-menu lang no-padding no-margin languages']//li[1]//a")
+    WebElement fieldEnglishLang;
+
+
+    public LocaleVerificator(WebDriver driver) {
+        super(driver);
+    }
+
+    public  String getLanguage()
+    {
+        return fieldLang.getText();
+    }
+
+    public void setLanguage()
+    {
+        String lang = "English";
+        if(!getLanguage().equals(lang)) {
+            fieldLang.click();
+            fieldEnglishLang.click();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        }
+
+    }
+
+
+}
