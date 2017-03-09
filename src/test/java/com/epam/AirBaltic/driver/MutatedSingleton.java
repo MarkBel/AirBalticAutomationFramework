e945caf63d3b3b6643edd2f45e5ca567f4b8adce
shaman2001 <Vladimir_Kotovich@epam.com>
2017-03-09 17:23:06 +0300

    class MutatedSingleton simplified

--- src/test/java/com/epam/AirBaltic/driver/MutatedSingleton.java
+++ src/test/java/com/epam/AirBaltic/driver/MutatedSingleton.java
@@ -3,52 +3,31 @@ package com.epam.AirBaltic.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class MutatedSingleton {
 
-    private static Map<String, WebDriver> driverList = new HashMap<>();
+    private static WebDriver webDriver;
 
     public static WebDriver getDriver(DesiredCapabilities cap) {
-        WebDriver driver;
         String browserName = cap.getBrowserName();
         switch (browserName) {
             case "chrome":
-                    driver = new ChromeDriver(cap);
-                    driverList.put("chrome", driver);
+                webDriver = new ChromeDriver(cap);
                 break;
             case "firefox":
-                    driver = new FirefoxDriver(cap);
-                    driverList.put("firefox", driver);
+                webDriver = new FirefoxDriver(cap);
                 break;
             default:
-                    driver = new OperaDriver(cap);
-                    driverList.put("opera", driver);
+                webDriver = new FirefoxDriver(cap);
                 break;
         }
-        return driverList.get(browserName);
+        return webDriver;
     }
 
-    private static boolean isAlreadyRuns(String driverName) {
-        boolean result = false;
-        for (String key: driverList.keySet()) {
-            if (key.equals(driverName)) {
-                result = true;
-                break;
-            }
-        }
-        return result;
+    public static void cLoseDriver() {
+        webDriver.quit();
+        webDriver = null;
     }
-
-    public static void cLoseAllDrivers() {
-        for (WebDriver wd: driverList.values()) {
-            wd.quit();
-            wd = null;
-        }
-    }
+}
 }
