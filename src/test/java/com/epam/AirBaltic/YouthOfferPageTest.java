64d8461d9ae6a8dc7504b52afeeedac078cbe419
shaman2001 <Vladimir_Kotovich@epam.com>
2017-03-09 18:02:45 +0300

    YouthOfferPageTest remarks fixed

--- src/test/java/com/epam/AirBaltic/YouthOfferPageTest.java
+++ src/test/java/com/epam/AirBaltic/YouthOfferPageTest.java
@@ -15,20 +15,12 @@ public class YouthOfferPageTest extends AbstractPageTest {
 
     @BeforeClass
     public void jumpToYouthOfferPage() {
-//        if (!this.startPage.isLoggedIn()) {
-//           this.startPage.loginFromStartPage();
-//        }
         youthOfferPage = this.startPage.gotoYouthOfferPageByLink();
-        //logger.info("Page YouthOfferPage opened");
     }
 
     @Test(dataProvider="citiesList")
     public void testOffersListCorrectlyShown(String cityName, boolean expectedResult) {
-        //logger.info("setting origin city in menu as " + cityName);
         youthOfferPage.setOriginCity(cityName);
-        cityName = youthOfferPage.getSelectedOriginCity();
-        //logger.info("origin city in menu - " + cityName);
-        //logger.info("Number of offers from " + cityName + " is " + youthOfferPage.getOffersNumberFromCurrentCity());
         Assert.assertEquals(youthOfferPage.isOriginsShownCorrectly(), expectedResult);
     }
 
