2593c6bc27649d54ef1e7f5e782fb7fe75fc3640
shaman2001 <Vladimir_Kotovich@epam.com>
2017-03-09 12:41:05 +0300

    Added check for element presence to Page class

--- src/test/java/com/epam/AirBaltic/pages/Page.java
+++ src/test/java/com/epam/AirBaltic/pages/Page.java
@@ -1,10 +1,10 @@
 package com.epam.AirBaltic.pages;
 
 import com.epam.AirBaltic.util.FluentWaitUtil;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Abstract class representation of a Page in the UI. Page object pattern
@@ -13,7 +13,6 @@ public abstract class Page {
 
     protected WebDriver driver;
     protected FluentWaitUtil wait;
-
 
     public Page(WebDriver driver) {
         this.driver = driver;
@@ -37,6 +36,17 @@ public abstract class Page {
     public void jsExecutorWork(WebElement webElement) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", webElement);
+    }
+
+    public boolean isElementPresent(By by) {
+        try {
+            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            this.driver.findElement(by);
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
 }
