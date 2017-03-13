df055cb7f8e2cad4f157fc42c05a75e0799f09db
Kseniya_Kunda <Ksenia.Kunda@gmail.com>
2017-03-13 12:35:31 +0300

    change closePopUpWindow()

--- src/test/java/com/epam/AirBaltic/pages/Page.java
+++ src/test/java/com/epam/AirBaltic/pages/Page.java
@@ -79,6 +79,7 @@ public abstract class Page {
             WebElement element = driver.findElement(buttonClose);
             element.click();
         }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     public static boolean isElementPresent(By by,WebDriver driver)
